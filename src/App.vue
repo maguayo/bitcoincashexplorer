10cfab1975413c37fb3e45880674385c063e932f
Marcos Aguayo <marcos@aguayo.es>
2019-05-01 11:09:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg: "Hello World"
+    }
   }
 }
 </script>
