e1058d78066db7558de9990c85ccf32269cd8da6
Marcos Aguayo <marcos@aguayo.es>
2019-05-03 15:39:05 +0200

    Mempool widgets

--- src/views/Mempool.vue
+++ src/views/Mempool.vue
@@ -2,14 +2,32 @@
     <div id="app">
         <Header />
         <div class="container">
-            <h4 class="text-left">Mempool transactions: </h4>
-            <table class="table table-striped table-bordered">
-              <tbody>
-                <tr v-for="tx in mempool" :key="tx">
-                  <td>{{ tx }}</td>
-                </tr>
-              </tbody>
-            </table>
+            <section class="transaction-widgets">
+              <div class="row">
+                <div class="widget col-md-6">
+                  <div class="widget-inner" style="background-color: #d6535c;">
+                    <p>Transactions</p>
+                    <span>{{mempoolInfo.size}}</span>
+                  </div>
+                </div>
+                <div class="widget col-md-6">
+                  <div class="widget-inner" style="background-color: #9d8bf7;">
+                    <p>Size</p>
+                    <span>{{mempoolInfo.bytes}}</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="raw-transaction-list">
+              <h4 class="text-left">Mempool transactions: </h4>
+              <table class="table table-striped table-bordered">
+                <tbody>
+                  <tr v-for="tx in mempool" :key="tx">
+                    <td>{{ tx }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section>
         </div>
     </div>
 </template>
@@ -21,6 +39,7 @@ let BITBOXSDK = require('bitbox-sdk');
 let BITBOX = new BITBOXSDK();
 
 let mempool = [];
+let mempoolInfo = {"size": "-", "bytes": "-"}
 
 export default {
   name: 'Mempool',
@@ -29,29 +48,47 @@ export default {
   },
   data: function () {
     return {
-      mempool: mempool
+      mempool: mempool,
+      mempoolInfo: mempoolInfo
     }
   },
   methods: {
     getRawMempool: function () {
-      BITBOX.Blockchain.getRawMempool(true).then( mempool => {
+      BITBOX.Blockchain.getRawMempool(true).then(mempool => {
         this.mempool = mempool;
+      });
+    },
+    getMempoolInfo: function() {
+      BITBOX.Blockchain.getMempoolInfo().then(mempoolInfo => {
+        this.mempoolInfo = mempoolInfo;
       });
     }
   },
   mounted: function () {
-    this.getRawMempool()
+    this.getRawMempool();
+    this.getMempoolInfo();
   }
 }
 
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.transaction-widgets{
+  margin-bottom: 50px;
+}
+.widget-inner{
+  border: 1px solid #ccc;
+  padding: 35px 20px;
+  color: #fff;
+}
+.widget-inner p{
+  margin: 0px;
+  font-size: 15px;
+  text-transform: uppercase;
+}
+.widget-inner span{
+  font-size: 22px;
+  margin-top: 5px;
+  display: block;
 }
 </style>
